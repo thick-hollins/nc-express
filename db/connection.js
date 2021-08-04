aa45a433ea548681f8fc35aed5ab58de1f9d9fef
thick-hollins <alexnuttall@zoho.eu>
2021-08-04 13:27:37 +0100

    heroku setup

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,22 @@ const { Pool } = require('pg');
 const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
 require('dotenv').config({
   path: path.resolve(__dirname, `../.env.${ENV}`),
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);

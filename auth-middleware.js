353d828fb10584c7bf09b71a4dea808afecc613a
thick-hollins <th@DESKTOP-9GPF8TB.localdomain>
2021-08-15 17:28:57 +0100

    middleware

--- auth-middleware.js
+++ auth-middleware.js
@@ -7,9 +7,9 @@ exports.authoriseRequest = (req, res, next) => {
       return next()
     } else {
       const { headers: { authorization } } = req
+      if (!authorization.startsWith('BEARER')) return next({ status: 401, msg: 'Unauthorised' })
       const token = authorization.split(' ')[1]
       const decoded = jwt.decode(token)
-      if (!decoded) return next({ status: 401, msg: 'Unauthorised' })
       const { username, iat } = decoded
   
       client.get(username, (err, data) => {
--- db/redis-connection.js
+++ db/redis-connection.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const client = redis.createClient(process.env.REDIS_URL || null);
+const client = redis.createClient(process.env.REDIS_URL)
 
 client.on("error", (error) => {
   console.error(error);

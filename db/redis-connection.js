111bd97ed6ab4009f07c8dcf8752f1e28e1b62fd
thick-hollins <alexnuttall@zoho.eu>
2021-08-15 15:19:07 +0100

    redis connection

--- db/redis-connection.js
+++ db/redis-connection.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_URL || null);
 
 client.on("error", (error) => {
   console.error(error);

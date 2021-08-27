eb86f98960791fe6ed488836f01369065757e7a1
thick-hollins <alexnuttall@zoho.eu>
2021-08-27 14:04:55 +0100

    add dev data passwords

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,45 +3,72 @@ module.exports = [
     username: 'tickle122',
     name: 'Tom Tickle',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
+    'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+    'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+    'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
+    'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
+    'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
+    'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
+    admin: false,
+    salt: 'f7df80e8ed903d8a1e78e62bd7f482fa',
+    hash: 'd512908edf90bd51150e3ae8f3eac8148b338498df3d768199141697ceff06c489ca8c432663284c8ae87be2874ad51ae2af8adc27057f38a29996123837742f'
   },
   {
     username: 'sonic_hedgehog',
     name: 'Alex',
     avatar_url:
-      'https://www.letsplaysega.com/wp-content/uploads/images/gen/Sonic_the_Hedgehog.png',
+    'https://www.letsplaysega.com/wp-content/uploads/images/gen/Sonic_the_Hedgehog.png',
     admin: true,
     salt: '32fbb8e1cbb4be241105139bb55271f1',
     hash: 'de77526cf67eeb57bb722d8d1f4e5f2394f13f7a1c5a76ff69b5d49e509d4318fb6e2562154657b69df242ee89774687a9d7c315db3a2d8b9268243aa86bebbf'
+  },
+  {
+    username: 'luigi',
+    name: 'Normal User',
+    avatar_url:
+    'https://upload.wikimedia.org/wikipedia/en/7/73/Luigi_NSMBUDX.png',
+    admin: false,
+    salt: '26a9d9c898567c2372d0bf97a91e7693',
+    hash: '03191790273d674f18a9ea64becd9d5189258ae14207ef4d867d048fbaa27352e6a2600d32a8a8e5bd60f280f23a425f7a1b94d782a87fab1d0d96e1a2728326'
   }
 ];
